2d7cd1f2f526fb5a7dae3770167b0bfc48497cbb
sun2snow <77017505@qq.com>
2025-05-06 18:03:52 +0800

    Create Std.cpp

---
+++ AcWing/482/Std.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+const int N = 110;
+
+int n, ans, a[N], f[N], g[N];
+
+int main() {
+  std::cin >> n;
+  for (int i = 1; i <= n; ++i) std::cin >> a[i];
+  for (int i = 1; i <= n; ++i) {
+    f[i] = 1;
+    for (int j = 1; j < i; ++j)
+      if (a[j] < a[i]) f[i] = std::max(f[i], f[j] + 1);
+  }
+  for (int i = n; i; --i) {
+    g[i] = 1;
+    for (int j = i + 1; j <= n; ++j)
+      if (a[i] > a[j]) g[i] = std::max(g[i], g[j] + 1);
+  }
+  for (int i = 1; i <= n; ++i)
+    ans = std::max(ans, f[i] + g[i] - 1);
+  std::cout << n - ans << std::endl;
+  return 0;
+}
